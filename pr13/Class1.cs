7b57702d5ff642b9b71522929bf7d9774dfaae4f
ИСП-31 <dfyrkin@bk.ru>
2023-12-08 14:56:16 +0300

    Добавьте файлы проекта.

---
+++ pr13/Class1.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr13
+{
+    public class Soluiton
+    {
+        /// <summary>
+        /// находит номер последнего столбца, содержащего равное количество положительных и отрицательных элементов
+        /// </summary>
+        /// <param name="matrix">матрица</param>
+        /// <returns>номер последнего столбца, содержащего равное количество положительных и отрицательных элементов</returns>
+        public static int GetResult(int[,] matrix)
+        {
+            int count1, count2, result = 0;
+
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                count1 = 0; count2 = 0;
+
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    if (matrix[i, j] < 0) count1++;
+                    else count2++;
+                }
+
+                if (count1 == count2) result = j + 1;
+            }
+
+            return result;
+        }
+    }
+
+    public class ArrayMod
+    {
+        public static void FillRandon(ref int[,] matrix)
+        {
+            Random rnd = new();
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = rnd.Next(-10, 10);
+                }
+            }
+        }
+
+        public static void Save(int[,] mas)
+        {
+            SaveFileDialog save = new();
+            save.DefaultExt = ".txt";
+            save.Filter = "Все файлы (*.*) | *.* | Текстовые файлы | *.txt";
+            save.FilterIndex = 2;
+            save.Title = "Сохранение таблицы";
+
+            if (save.ShowDialog() == true)
+            {
+                StreamWriter file = new(save.FileName);
+                file.WriteLine(mas.GetLength(0));
+                file.WriteLine(mas.GetLength(1));
+
+                foreach (int i in mas)
+                {
+                    file.WriteLine(i);
+                }
+                file.Close();
+            }
+        }
+
+        public static int[,]? Open()
+        {
+            OpenFileDialog open = new();
+            open.DefaultExt = ".txt";
+            open.Filter = "Все файлы (*.*) | *.* | Текстовые файлы | *.txt";
+            open.FilterIndex = 2;
+            open.Title = "Открытие таблицы";
+
+            if (open.ShowDialog() == true)
+            {
+                StreamReader file = new(open.FileName);
+
+                int length1 = Convert.ToInt32(file.ReadLine());
+                int length2 = Convert.ToInt32(file.ReadLine());
+
+                int[,] mas = new int[length1, length2];
+
+                for (int i = 0; i < length1; i++)
+                {
+                    for (int j = 0; j < length2; j++)
+                    {
+                        mas[i, j] = Convert.ToInt32(file.ReadLine());
+                    }
+                }
+                file.Close();
+                return mas;
+            }
+            return null;
+
+        }
+
+    }
+
+    public static class VisualArray
+    {
+        public static DataTable ToDataTable<T>(this T[] arr)
+        {
+            var res = new DataTable();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                res.Columns.Add("col" + (i + 1), typeof(T));
+            }
+            var row = res.NewRow();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                row[i] = arr[i];
+            }
+            res.Rows.Add(row);
+            return res;
+        }
+        public static DataTable ToDataTable<T>(this T[,] matrix)
+        {
+            var res = new DataTable();
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                res.Columns.Add("col" + (i + 1), typeof(T));
+            }
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                var row = res.NewRow();
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    row[j] = matrix[i, j];
+                }
+
+                res.Rows.Add(row);
+            }
+
+            return res;
+        }
+    }
+}
---
+++ pr13/MainWindow.xaml.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace pr13
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        int[,] matrix;
+
+        DispatcherTimer timer;
+        
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            timer = new();
+            timer.Tick += Timer_Tick;
+            timer.Interval = new(0, 0, 0, 1, 0);
+            timer.IsEnabled = true;
+
+            tbNumber.Text = "Практическая работа №13";
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            DateTime date = DateTime.Now;
+            tbTime.Text = date.ToString("HH:mm:ss");
+            tbDate.Text = date.ToString("dd.MM.yyyy");
+        }
+
+        private void btnFIll_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                ArrayMod.FillRandon(ref matrix);
+                dataGid.ItemsSource = VisualArray.ToDataTable(matrix).DefaultView;
+            }
+            catch
+            {
+                MessageBox.Show("Таблица не была создана");
+            }
+        }
+
+        private void btnCreate_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int n = Convert.ToInt32(tbN.Text),
+                    m = Convert.ToInt32(tbM.Text);
+
+                matrix = new int[m, n];
+
+                dataGid.ItemsSource = VisualArray.ToDataTable(matrix).DefaultView;
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Вводите только целочисленные значения");
+            }
+            catch
+            {
+                MessageBox.Show("Что-то пошло не так");
+            }
+        }
+
+        private void btnResult_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int result = Soluiton.GetResult(matrix);
+
+                tbResult.Text = result.ToString();
+            }
+            catch (NullReferenceException)
+            {
+                MessageBox.Show("Таблица не была создана");
+            }
+            catch
+            {
+                MessageBox.Show("Что-то пошло не так");
+            }
+        }
+
+        private void btnInfo_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("О программе");
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                ArrayMod.Save(matrix);
+            }
+            catch (NullReferenceException)
+            {
+                MessageBox.Show("Таблица не была создана");
+            }
+        }
+
+        private void btnOpen_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                matrix = ArrayMod.Open();
+                dataGid.ItemsSource = VisualArray.ToDataTable(matrix).DefaultView;
+            }
+            catch
+            {
+                MessageBox.Show("Что-то пошло не так");
+            }
+        }
+
+        private void dataGrid_CellEditing(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            try
+            {
+                int index0 = e.Column.DisplayIndex;
+                int index1 = e.Row.GetIndex();
+
+                int value = int.Parse(((TextBox)e.EditingElement).Text);
+                matrix[index1, index0] = value;
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Вводите только целочисленные значения");
+            }
+            catch
+            {
+                MessageBox.Show("Что-то пошло не так");
+            }
+
+            tbResult.Clear();
+        }
+
+        private void btnParameters_Click(object sender, RoutedEventArgs e)
+        {
+            Parameters pramWindow = new();
+            pramWindow.Show();
+        }
+    }
+}
